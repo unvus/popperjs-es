9d3ae31c5f03ed509b76efc9e7780e4b9caa2fe2
三咲智子 <sxzz@sxzz.moe>
2022-04-22 09:18:09 +0800

    feat: add build

---
+++ build/build.ts
@@ -0,0 +1,34 @@
+/* eslint-disable */
+
+import { rollup } from 'rollup';
+import { babel } from '@rollup/plugin-babel';
+import esbuild from 'rollup-plugin-esbuild';
+
+const bundle = await rollup({
+  input: ['src/index.js'],
+  plugins: [
+    babel(),
+    esbuild({
+      target: 'esnext',
+      minify: true,
+      define: {
+        'process.env.NODE_ENV': 'production',
+      },
+    }),
+  ],
+});
+await Promise.all([
+  await bundle.write({
+    file: 'dist/index.js',
+    format: 'cjs',
+  }),
+  bundle.write({
+    file: 'dist/index.mjs',
+    format: 'esm',
+  }),
+  bundle.write({
+    file: 'dist/index.iife.js',
+    format: 'iife',
+    name: 'Popper',
+  }),
+]);
